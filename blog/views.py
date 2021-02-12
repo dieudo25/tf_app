8d2b19c97286650c465b8e56d3107c799fc57851
Dieudo25 <twahirwa25@gmail.com>
2021-02-12 21:22:57 +0100

    update blog app : add filter to PostPageSet view

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,22 @@ class PostPageSet(viewsets.ModelViewSet):
     queryset = PostPage.objects.all()
     http_method_names = ["get"]
 
+    def get_queryset(self):
+        """
+            Fonction that filter the queryset of PostPage model instance
+            By category from BlogCategory Model or by tag from Tag model
+        """
+        queryset = PostPage.objects.all()
+        category = self.request.query_params.get("category", None)
+        tag = self.request.query_params.get("tag", None)
+
+        if category is not None and category != "*":
+            queryset = queryset.filter(categories__blog_category__slug=category)
+        if tag is not None and tag != "*":
+            queryset = queryset.filter(tags__slug=tag)
+
+        return queryset
+
 
 class CategorySet(viewsets.ModelViewSet):
     """
