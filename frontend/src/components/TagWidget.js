c2ceaa7c914938422227e385fafeeaf957e41b21
Dieudonne Twahirwa <twahirwa25@gmail.com>
2021-02-14 19:25:56 +0100

    update frontend app

---
+++ frontend/src/components/TagWidget.js
@@ -0,0 +1,16 @@
+/* import React from "react";
+import "../index.scss";
+
+class TagWidget extends React.Component {
+  render() {
+    return (
+      <div className="card my-4">
+        <h5 className="card-header">Tags</h5>
+        <div className="card-body">Loading...</div>
+      </div>
+    );
+  }
+}
+
+export { TagWidget };
+ */
---
+++ frontend/src/stories/TagWidget.stories.js
@@ -0,0 +1,21 @@
+/* import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { TagWidget } from "../components/TagWidget";
+
+export default {
+  title: "TagWidget",
+  component: TagWidget,
+};
+
+export const Example = () => {
+  return (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <TagWidget />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+ */
