b04e38561168dd10585cdd9220d6e6077cc0b49f
Dieudonne Twahirwa <twahirwa25@gmail.com>
2021-02-15 22:59:05 +0100

    create PostPageCard component + storie

---
+++ frontend/src/stories/PostPageCard.stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { MemoryRouter } from "react-router-dom";
+import { PostPageCard } from "../components/PostPageCard";
+
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+import { mockPost } from "./mockUtils";
+
+export default {
+  title: "PostPageCard",
+  component: PostPageCard,
+};
+
+export const Example = () => {
+  const mock = new MockAdapter(axios);
+  mockPost(mock);
+
+  return (
+    <Container>
+      <Row>
+        <Col md={8}>
+          <MemoryRouter>
+            <PostPageCard postPk={5} />
+          </MemoryRouter>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
